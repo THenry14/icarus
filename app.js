3ea57d02145806feb4af26e450947efd033139e5
Łukasz Żarnowiecki <dolohow@outlook.com>
2014-11-23 17:47:55 +0100

    Implement passwordless - first stage
    There is still work to do:
    - the restricted access is always restricted
    - need some refactoring, it's just a demonstration that it actually
      works

--- app.js
+++ app.js
@@ -2,17 +2,48 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var passwordless = require('passwordless');
+var MongoStore = require('passwordless-mongostore');
+var email   = require("emailjs");
+
+var router = require('./routes/passwordless');
+
+var credentials = require('./credentials');
+
+var smtpServer  = email.server.connect(credentials.email);
+var pathToMongoDb = 'mongodb://localhost/passwordless-simple-mail';
+passwordless.init(new MongoStore(pathToMongoDb), { allowTokenReuse: true });
+
+passwordless.addDelivery(
+  function(tokenToSend, uidToSend, recipient, callback) {
+    var host = 'localhost:3000';
+    smtpServer.send({
+      text:    'Hello!\nAccess your account here: http://'
+      + host + '?token=' + tokenToSend + '&uid='
+      + encodeURIComponent(uidToSend),
+      from:    credentials.email.user,
+      to:      recipient,
+      subject: 'Token for ' + host
+    }, function(err, message) {
+      if(err) {
+        console.log(err);
+      }
+      callback(err);
+    });
+  }, { ttl: 1000*60*10 });
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+
+app.use(passwordless.acceptToken());
+app.use('/api', router);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
